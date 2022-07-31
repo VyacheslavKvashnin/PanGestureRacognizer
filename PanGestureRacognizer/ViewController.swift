0dfdb6d1d700864f0362c4cd7bbddfbc8b9e1ec2
komplektan <komplektan@mail.ru>
2022-07-31 22:53:43 +0500

    Start and finish project

--- PanGestureRacognizer/ViewController.swift
+++ PanGestureRacognizer/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let viewRect: UIView = {
+       let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+        view.backgroundColor = .red
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+        
+        view.addSubview(viewRect)
+        viewRect.center = view.center
+        
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(wasDragged))
+        panRecognizer.view?.isUserInteractionEnabled = true
+        view.addGestureRecognizer(panRecognizer)
     }
+    
+    @objc func wasDragged(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.view)
 
-
+        // Use translation.y to change the position of your customView, e.g.
+        viewRect.center.y = translation.y
+        viewRect.center.x = translation.x
+    }
 }
 
